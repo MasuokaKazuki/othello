e63254c8056d3f81edda16a94901f5b787c0a7b1
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-08-11 15:50:06 +0900

    命名を変更

--- src/main/java/othello/controller/ApiController.java
+++ src/main/java/othello/controller/ApiController.java
@@ -1,14 +1,33 @@
 package othello.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-/**
- * サンプルRestControllerクラス
- */
-@RestController("/")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import othello.model.BoardModel;
+import othello.repository.BoardRepository;
+
+@Controller
+@RequestMapping(path="/api/v1")
 public class ApiController {
-    @RequestMapping
-    public String hello() {
-        return "Hello Again!";
-    }
+  @Autowired
+  private BoardRepository boardRepository;
+
+  @PostMapping(path="/board/put")
+  public @ResponseBody String boardPut(@RequestParam String pieces, @RequestParam String player) {
+    BoardModel board = new BoardModel();
+    board.setPieces(pieces);
+    board.setPlayer(player);
+    boardRepository.save(board);
+    return "Saved";
+  }
+
+  @GetMapping(path="/all")
+  public @ResponseBody Iterable<BoardModel> getAllUsers() {
+    return boardRepository.findAll();
+  }
 }
