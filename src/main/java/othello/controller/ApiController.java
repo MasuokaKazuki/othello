bce8b895a412815c2ce832f97839c1a0ea8d16e1
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-08-13 11:51:30 +0900

    API側でCrosOriginを許可

--- src/main/java/othello/controller/ApiController.java
+++ src/main/java/othello/controller/ApiController.java
@@ -1,6 +1,7 @@
 package othello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import othello.model.BoardModel;
 import othello.service.BoardService;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path="/api/v1")
 public class ApiController {  
   @Autowired
@@ -22,12 +24,10 @@ public class ApiController {
   public @ResponseBody ResetResult boardReset() {
 	  return boardService.reset();
   }
-  
   @GetMapping(path="/board/put")
   public @ResponseBody PutResult boardPut(@RequestParam("x")Integer x, @RequestParam("y")Integer y) {	  
 	  return boardService.put(x,y);
   }
-
   @GetMapping(path="/board/")
   public @ResponseBody BoardModel display() {
 	  return boardService.getBoardData();
