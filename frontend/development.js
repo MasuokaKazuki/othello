237036dd2d0cec81b23bb099e19b8f53c269e1fc
KazukiMasuoka <masuoka.kazuki@gmail.com>
2020-08-14 14:52:51 +0900

    フロントエンド周りのファイルを移動

--- frontend/development.js
+++ frontend/development.js
@@ -2,7 +2,7 @@ import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const src  = path.resolve(__dirname, 'src')
-const dist = path.resolve(__dirname, 'dist')
+const dist = path.resolve(__dirname, '../src/main/resources/public')
 
 export default {
   mode: 'development',
